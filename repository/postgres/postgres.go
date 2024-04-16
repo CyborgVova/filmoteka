7e6e79e814cfbca2c55056b2aade20e0215bde2b
CyborgVova <eroxin79@mail.ru>
2024-04-17 01:09:00 +0300

    no working add new film or actor, TODO: why

--- repository/postgres/postgres.go
+++ repository/postgres/postgres.go
@@ -2,7 +2,6 @@ package postgres
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -77,26 +76,6 @@ func (r *Repository) GetFilmInfo(ctx context.Context, title string) ([]entities.
 	return films, nil
 }
 
-func (r *Repository) getAllFilmActors(ctx context.Context, filmid int) []entities.Actor {
-	rows, err := r.Conn.Query(ctx, "SELECT actors.* FROM actors "+
-		"JOIN films_actors ON films_actors.actor_id = actors.id "+
-		"JOIN films ON films.id = films_actors.film_id "+
-		"WHERE films.id = $1", filmid)
-	if err != nil {
-		log.Fatalf("error getting all film actors: %v\n", err)
-		return nil
-	}
-	defer rows.Close()
-
-	actors := []entities.Actor{}
-	for rows.Next() {
-		actor := entities.Actor{}
-		rows.Scan(&actor.ID, &actor.FullName, &actor.Sex, &actor.DateOfBirth)
-		actors = append(actors, actor)
-	}
-	return actors
-}
-
 func (r *Repository) GetActorInfo(ctx context.Context, fullname string) ([]entities.Actor, error) {
 	rows, err := r.Conn.Query(ctx,
 		fmt.Sprintf("SELECT * FROM actors "+
@@ -120,6 +99,101 @@ func (r *Repository) GetActorInfo(ctx context.Context, fullname string) ([]entit
 	return actors, nil
 }
 
+func (r *Repository) AddFilm(ctx context.Context, film entities.Film) (int, error) {
+	id := r.findFilm(ctx, film)
+	if id == 0 {
+		r.Conn.QueryRow(ctx, fmt.Sprintf("INSERT INTO films (title, description, release, rating) "+
+			"VALUES ('%s', '%s', '%d', '%d') RETURNING ID",
+			film.Title, film.Description, film.Release, film.Rating)).Scan(&id)
+	} else {
+		film.ID = id
+	}
+
+	r.fillActorsByFilm(ctx, film)
+	return id, nil
+}
+
+func (r *Repository) AddActor(ctx context.Context, actor entities.Actor) (int, error) {
+	id := r.findActor(ctx, actor)
+	if id != 0 {
+		r.Conn.QueryRow(ctx, fmt.Sprintf("INSERT INTO actors (fullname, sex, dateofbirth) "+
+			"VALUES ('%s', '%s', '%s') RETURNING id",
+			actor.FullName, actor.Sex, actor.DateOfBirth)).Scan(&id)
+	} else {
+		actor.ID = id
+	}
+
+	r.fillFilmsByActor(ctx, actor)
+	return id, nil
+}
+
+func (r *Repository) SetFilmInfo(ctx context.Context, film entities.Film) bool {
+	return true
+}
+
+func (r *Repository) SetActorInfo(ctx context.Context, actor entities.Actor) bool {
+	return false
+}
+
+func (r *Repository) findFilm(ctx context.Context, film entities.Film) (id int) {
+	r.Conn.QueryRow(ctx, "SELECT id FROM films "+
+		"WHERE films.title = $1 AND films.release = $2",
+		film.Title, film.Release).Scan(&id)
+	return
+}
+
+func (r *Repository) findActor(ctx context.Context, actor entities.Actor) (id int) {
+	r.Conn.QueryRow(ctx, "SELECT id FROM actors "+
+		"WHERE actors.fullname = $1 AND actors.dateofbirth = $2",
+		actor.FullName, actor.DateOfBirth).Scan(&id)
+	return
+}
+
+func (r *Repository) fillFilmsByActor(ctx context.Context, actor entities.Actor) {
+	for _, film := range actor.Films {
+		id, _ := r.AddFilm(ctx, film)
+		r.addFilmsActors(ctx, id, actor.ID)
+	}
+}
+
+func (r *Repository) fillActorsByFilm(ctx context.Context, film entities.Film) {
+	for _, actor := range film.Actors {
+		id, _ := r.AddActor(ctx, actor)
+		r.addFilmsActors(ctx, film.ID, id)
+	}
+}
+
+func (r *Repository) addFilmsActors(ctx context.Context, filmID, actorID int) {
+	var count int
+	r.Conn.QueryRow(ctx, "SELECT count(*) FROM films_actors "+
+		"WHERE film_id = $1 AND actor_id = $2",
+		filmID, actorID).Scan(&count)
+	if count == 0 {
+		r.Conn.QueryRow(ctx, fmt.Sprintf("INSERT INTO films_actors(film_id, actor_id) "+
+			"VALUES ('%d', '%d')", filmID, actorID))
+	}
+}
+
+func (r *Repository) getAllFilmActors(ctx context.Context, filmid int) []entities.Actor {
+	rows, err := r.Conn.Query(ctx, "SELECT actors.* FROM actors "+
+		"JOIN films_actors ON films_actors.actor_id = actors.id "+
+		"JOIN films ON films.id = films_actors.film_id "+
+		"WHERE films.id = $1", filmid)
+	if err != nil {
+		log.Fatalf("error getting all film actors: %v\n", err)
+		return nil
+	}
+	defer rows.Close()
+
+	actors := []entities.Actor{}
+	for rows.Next() {
+		actor := entities.Actor{}
+		rows.Scan(&actor.ID, &actor.FullName, &actor.Sex, &actor.DateOfBirth)
+		actors = append(actors, actor)
+	}
+	return actors
+}
+
 func (r *Repository) getAllFilmsActor(ctx context.Context, actorid int) []entities.Film {
 	rows, err := r.Conn.Query(ctx, "SELECT films.* from films "+
 		"JOIN films_actors ON films_actors.film_id = films.id "+
@@ -136,90 +210,4 @@ func (r *Repository) getAllFilmsActor(ctx context.Context, actorid int) []entiti
 		films = append(films, film)
 	}
 	return films
-}
-
-func (r *Repository) fillActorsByFilm(ctx context.Context, film entities.Film) {
-	for _, actor := range film.Actors {
-		id, _ := r.AddActor(ctx, actor)
-		if id == 0 {
-			continue
-		}
-		r.Conn.QueryRow(ctx, fmt.Sprintf("INSERT INTO films_actors(film_id, actor_id) "+
-			"VALUES ('%d', '%d')", film.ID, id))
-	}
-}
-
-func (r *Repository) AddFilm(ctx context.Context, film entities.Film) (int, error) {
-	if r.findFilm(ctx, film) != 0 {
-		r.fillActorsByFilm(ctx, film)
-		return 0, errors.New("such film exist yet")
-	}
-
-	row := r.Conn.QueryRow(ctx, fmt.Sprintf("INSERT INTO films (title, description, release, rating) "+
-		"VALUES ('%s', '%s', '%d', '%d') RETURNING ID",
-		film.Title, film.Description, film.Release, film.Rating))
-	var id int
-	row.Scan(&id)
-	if id == 0 {
-		return 0, errors.New("error insert a film")
-	}
-	r.fillActorsByFilm(ctx, film)
-	return id, nil
-}
-
-func (r *Repository) findFilm(ctx context.Context, film entities.Film) (id int) {
-	row := r.Conn.QueryRow(ctx, "SELECT id FROM films "+
-		"WHERE films.title = $1 AND films.release = $2", film.Title, film.Release)
-	row.Scan(&id)
-	return
-}
-
-func (r *Repository) fillFilmsByActor(ctx context.Context, actor entities.Actor) {
-	for _, film := range actor.Films {
-		id, _ := r.AddFilm(ctx, film)
-		if id == 0 {
-			continue
-		}
-		r.Conn.QueryRow(ctx, fmt.Sprintf("INSERT INTO films_actors(film_id, actor_id) "+
-			"VALUES ('%d', '%d')", id, actor.ID))
-	}
-}
-
-func (r *Repository) AddActor(ctx context.Context, actor entities.Actor) (int, error) {
-	item := r.findActor(ctx, actor)
-	if item != 0 {
-		r.fillFilmsByActor(ctx, actor)
-		return 0, errors.New("such actor exist yet")
-	}
-
-	row := r.Conn.QueryRow(ctx, fmt.Sprintf("INSERT INTO actors (fullname, sex, dateofbirth) "+
-		"VALUES ('%s', '%s', '%s') RETURNING id",
-		actor.FullName, actor.Sex, actor.DateOfBirth))
-	var id int
-	row.Scan(&id)
-	if id == 0 {
-		return 0, errors.New("error insert an actor")
-	}
-	r.fillFilmsByActor(ctx, actor)
-	return id, nil
-}
-
-func (r *Repository) findActor(ctx context.Context, actor entities.Actor) (id int) {
-	rows, err := r.Conn.Query(ctx, "SELECT id FROM actors "+
-		"WHERE actors.fullname = $1 AND actors.dayofbirth = $2",
-		actor.FullName, actor.DateOfBirth)
-	if err != nil {
-		log.Fatal("error find actor identificators")
-	}
-
-	rows.Scan(&id)
-	return
-}
-
-func (r *Repository) SetFilmInfo(ctx context.Context, film entities.Film) bool {
-	return true
-}
-
-func (r *Repository) SetActorInfo(ctx context.Context, actor entities.Actor) bool {
-	return false
 }
